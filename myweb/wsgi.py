8ef1156091d7b0ce9c0c518279968068c321ce08
vachestantin <vachestantin@gmail.com>
2016-03-26 17:54:56 +0900

    배치 준비 완료..

--- myweb/wsgi.py
+++ myweb/wsgi.py
@@ -1,16 +1,18 @@
-"""
-WSGI config for myweb project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myweb.settings")
+env = os.environ.get('MYWEB_ENV', '')
+
+if env:
+    if not env.endswith('_settings'):
+        env ='{}_settings'.format(env)
+    else:
+        env = 'settings'
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "myweb.{}".format(env)
+)
 
 application = get_wsgi_application()
